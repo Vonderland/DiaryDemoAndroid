522367dce9739cd1a302c265c9b326cfea8ae535
Vonderland <897007204@qq.com>
2017-02-02 21:15:21 +0800

    加入 Moment 与 Diary 对应 adapter

--- app/src/main/java/com/vonderland/diarydemo/adapter/MomentAdapter.java
+++ app/src/main/java/com/vonderland/diarydemo/adapter/MomentAdapter.java
@@ -6,11 +6,10 @@ import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.vonderland.diarydemo.R;
-import com.vonderland.diarydemo.bean.Diary;
+import com.vonderland.diarydemo.bean.Moment;
 import com.vonderland.diarydemo.utils.DateTimeUtil;
 
 import java.util.List;
@@ -19,17 +18,17 @@ import java.util.List;
  * Created by Vonderland on 2017/2/2.
  */
 
-public class DiaryAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+public class MomentAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     private Context context;
     private LayoutInflater inflater;
-    private List<Diary> data;
+    private List<Moment> data;
 
-    private static final int VIEW_TYPE_DIARY = 1;
+    private static final int VIEW_TYPE_MOMENT = 1;
     private static final int VIEW_TYPE_LOADING = 2;
     private static final int VIEW_TYPE_NO_MORE = 3;
     private static final int VIEW_TYPE_EMPTY = 4;
 
-    public DiaryAdapter(Context context, List<Diary> data) {
+    public MomentAdapter(Context context, List<Moment> data) {
         this.context = context;
         this.inflater = LayoutInflater.from(context);
         this.data = data;
@@ -37,16 +36,16 @@ public class DiaryAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> 
 
     @Override
     public int getItemViewType(int position) {
-        Diary diary = data.get(position);
+        Moment moment = data.get(position);
         int viewType;
-        if (diary.isFooter()) {
+        if (moment.isFooter()) {
             viewType = VIEW_TYPE_LOADING;
-        } else if (diary.isEmptyView()) {
+        } else if (moment.isEmptyView()) {
             viewType = VIEW_TYPE_EMPTY;
-        } else if (diary.isNoMore()) {
+        } else if (moment.isNoMore()) {
             viewType = VIEW_TYPE_NO_MORE;
         } else {
-            viewType = VIEW_TYPE_DIARY;
+            viewType = VIEW_TYPE_MOMENT;
         }
         return viewType;
     }
@@ -54,32 +53,26 @@ public class DiaryAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> 
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         switch (viewType) {
-            case VIEW_TYPE_DIARY:
-                return new DiaryViewHolder(inflater.inflate(R.layout.item_diary, parent, false));
+            case VIEW_TYPE_MOMENT:
+                return new MomentViewHolder(inflater.inflate(R.layout.item_moment, parent, false));
             case VIEW_TYPE_EMPTY:
-                return new DiaryEmptyViewHolder(inflater.inflate(R.layout.item_diary_empty, parent, false));
+                return new MomentEmptyViewHolder(inflater.inflate(R.layout.item_moment_empty, parent, false));
             case VIEW_TYPE_LOADING:
                 return new LoadingFooterHolder(inflater.inflate(R.layout.item_footer, parent, false));
             case VIEW_TYPE_NO_MORE:
-                return new NoMoreDiaryViewHolder(inflater.inflate(R.layout.item_no_more, parent, false));
+                return new NoMoreMomentViewHolder(inflater.inflate(R.layout.item_no_more, parent, false));
             default:
-                return new DiaryViewHolder(inflater.inflate(R.layout.item_diary, parent, false));
+                return new MomentViewHolder(inflater.inflate(R.layout.item_moment, parent, false));
         }
     }
 
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-        if (holder instanceof DiaryViewHolder) {
-            Diary diary = data.get(position);
-
-            if (TextUtils.isEmpty(diary.getUrl())) {
-                ((DiaryViewHolder) holder).picture.setVisibility(View.GONE);
-            } else {
-                // TODO: 引入 picasso
-            }
-            ((DiaryViewHolder) holder).title.setText(diary.getTitle());
-            ((DiaryViewHolder) holder).description.setText(diary.getDescription());
-            ((DiaryViewHolder) holder).date.setText(DateTimeUtil.formatDate(diary.getEventTime()));
+        if (holder instanceof MomentViewHolder) {
+            Moment moment = data.get(position);
+            ((MomentViewHolder) holder).title.setText(moment.getTitle());
+            ((MomentViewHolder) holder).location.setText(moment.getLocation());
+            ((MomentViewHolder) holder).date.setText(DateTimeUtil.formatDate(moment.getEventTime()));
         }
     }
 
@@ -94,29 +87,27 @@ public class DiaryAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> 
         }
     }
 
-    public static class DiaryEmptyViewHolder extends RecyclerView.ViewHolder {
-        public  DiaryEmptyViewHolder(View itemView){
+    public static class MomentEmptyViewHolder extends RecyclerView.ViewHolder {
+        public  MomentEmptyViewHolder(View itemView){
             super(itemView);
         }
     }
 
-    public static class NoMoreDiaryViewHolder extends RecyclerView.ViewHolder {
-        public NoMoreDiaryViewHolder(View itemView){
+    public static class NoMoreMomentViewHolder extends RecyclerView.ViewHolder {
+        public NoMoreMomentViewHolder(View itemView){
             super(itemView);
         }
     }
 
-    public static class DiaryViewHolder extends RecyclerView.ViewHolder {
-        ImageView picture;
+    public static class MomentViewHolder extends RecyclerView.ViewHolder {
         TextView title;
-        TextView description;
+        TextView location;
         TextView date;
 
-        public DiaryViewHolder(View itemView) {
+        public MomentViewHolder(View itemView) {
             super(itemView);
-            picture = (ImageView)itemView.findViewById(R.id.picture);
             title = (TextView) itemView.findViewById(R.id.title);
-            description = (TextView) itemView.findViewById(R.id.description);
+            location = (TextView) itemView.findViewById(R.id.description);
             date = (TextView) itemView.findViewById(R.id.date);
         }
     }
